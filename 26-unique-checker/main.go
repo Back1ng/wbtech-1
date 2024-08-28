5b783dbba23c9fb6bca0ca762069175fb14197a1
Back1ng <anatolyu128@gmail.com>
2024-08-28 09:18:29 +0500

    initial readme

--- 26-unique-checker/main.go
+++ 26-unique-checker/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+// Разработать программу, которая проверяет,
+// что все символы в строке уникальные (true — если уникальные, false etc).
+// Функция проверки должна быть регистронезависимой.
+// Например:
+// abcd — true
+// abCdefAaf — false
+// aabcd — false
+func main() {
+	fmt.Println(isUnique("asDvf"))
+}
+
+func isUnique(str string) bool {
+	str = strings.ToLower(str)
+
+	for i := 0; i < len(str); i++ {
+		for j := i + 1; j < len(str); j++ {
+			if str[i] == str[j] {
+				return false
+			}
+		}
+	}
+
+	return true
 }
